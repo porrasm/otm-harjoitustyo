b00a0237fa1a2334e765974572e6b805672e9aa0
porrasm <31691452+porrasm@users.noreply.github.com>
2018-04-24 22:21:01 +0300

    muutoksia/siistintää

--- TexasHoldEmGame/Assets/Tests/TexasHoldemGameTest.cs
+++ TexasHoldEmGame/Assets/Tests/TexasHoldemGameTest.cs
@@ -6,46 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class TexasHoldemGameTest {
 
-
-    [UnityTest]
-    public IEnumerator GameInitializesCorrectly() {
-
-        SceneManager.LoadScene("default", LoadSceneMode.Single);
-
-        yield return new WaitForSeconds(1);
-
-        CustomNetworkManager networkManager = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<CustomNetworkManager>();
-        networkManager.StartHost();
-        yield return new WaitForSeconds(1);
-
-        Table table = GameObject.FindGameObjectWithTag("Table").GetComponent<Table>();
-        TexasHoldEm game = GameObject.FindGameObjectWithTag("Scripts").GetComponent<TexasHoldEm>();
-
-        Assert.IsNotNull(game);
-        Assert.IsNotNull(table);
-
-        yield return new WaitForSeconds(1);
-
-        game.PlaceHolderStart();
-
-        Assert.IsTrue(game.Players.Length == 1);
-        Assert.IsFalse(game.GameIsReady);
-
-        game.Players[0].Ready = true;
-
-        yield return new WaitForSeconds(2);
-
-        Assert.IsTrue(game.RoundIsOn);
-
-        // AI 
-        Assert.IsTrue(game.Players.Length == 10);
-
-        foreach (Player p in game.Players) {
-
-            Assert.IsTrue(p.Money == game.BuyIn);
-        }
-    }
-
     [UnityTest]
     public IEnumerator GameProgressesCorrectly() {
 
